f37c842cc7a2afbfa8c9c98897e693a03d291569
Paulo <37113@ufp.edu.pt>
2019-04-10 00:15:42 +0100

    Skills e alteraçao de arrayList

--- src/Projeto/Main.java
+++ src/Projeto/Main.java
@@ -1,16 +1,22 @@
 package Projeto;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main (String[] args)
     {
-        Date d1 = new Date(21,03,1996,23,00);
-        Date d2 = new Date(10,03,1990,11,00);
         Date d1r = new Date (10,05,2010,00,00);
         Location l1 = new Location(10,10);
         Location l2 = new Location(1,1);
         Location l3 = new Location(4,5);
-        Professional p1 = new Professional("Paulo",23,"Masculino",d1,d1r,l1);
-        Professional p2 = new Professional("Sheila",69,"Feminino",d2,d1r,l1);
+        ArrayList<String> skillsP1 = new ArrayList<>();
+        skillsP1.add("Programacao C");
+        skillsP1.add("Programacao JAVA");
+        ArrayList<String> skillsP2 = new ArrayList<>();
+        skillsP2.add("Fotografia");
+        skillsP2.add("Video");
+        Professional p1 = new Professional("Paulo",23,"Masculino",d1r,skillsP1,l1);
+        Professional p2 = new Professional("Sheila",69,"Feminino",d1r,skillsP2,l1);
         Company c1 = new Company("UFP",252252525,190190190,l2);
         Company c2 = new Company("PR MOTORS",123456789,423567437,l3);
 
--- src/Projeto/Skill.java
+++ src/Projeto/Skill.java
@@ -3,15 +3,39 @@ package Projeto;
 
 public class Skill {
 
-    public String name;
+    private String name;
 
-    public Professional professional;
+    private Professional professional;
 
     /**
      * Constructor
      */
     public Skill(String name, Professional professional) {
+        this.setName(name);
+        this.setProfessional(professional);
+    }
+
+    /**
+     * Gets and Sets
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public Professional getProfessional() {
+        return professional;
+    }
+
+    public void setProfessional(Professional professional) {
         this.professional = professional;
+    }
+
+    public void addSkilltoProfessional(Professional p)
+    {
+
     }
 }
