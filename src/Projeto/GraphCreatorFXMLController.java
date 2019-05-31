96098b5a503a897718ab64c0236a5d685981a169
Paulo <37113@ufp.edu.pt>
2019-05-31 11:30:19 +0100

    adicionei JavaFX

---
+++ src/Projeto/GraphCreatorFXMLController.java
@@ -0,0 +1,39 @@
+package Projeto;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GraphCreatorFXMLController implements Initializable {
+
+    public TableView disciplinesTable;
+    public TableColumn nameCol;
+    public TableColumn nifCol;
+    public TableColumn compCol;
+
+    public void handleReadBinFileAction(ActionEvent actionEvent) {
+    }
+
+    public void handleSaveBinFileAction(ActionEvent actionEvent) {
+    }
+
+    public void handleReadFileAction(ActionEvent actionEvent) {
+    }
+
+    public void handleSaveFileAction(ActionEvent actionEvent) {
+    }
+
+    public void handleExitAction(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
